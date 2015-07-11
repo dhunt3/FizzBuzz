1611c3f741c06e5b4af36eaecab7f92c08ae85d4
Noel <harrisbnoel@gmail.com>
2015-07-11 12:28:50 -0400

    fizz buzz code

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FizzBuzzDoro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizz = 3;
+            int buzz = 5;
+            int fizzBuzz = fizz * buzz;
+            int num = 15;
+            for (int counter = 1; counter <= num; counter++)
+            {
+                if (counter % fizzBuzz == 0)
+                {
+                    //System.Console.Write('\n');
+                    System.Console.Write("FB ");
+                }
+                else if (counter % fizz == 0)
+                {
+                   // System.Console.Write('\n');
+                    System.Console.Write("F ");
+                }
+                else if (counter % buzz == 0)
+                {
+                    //System.Console.Write('\n');
+                    System.Console.Write("B ");
+                }
+                else
+                {
+                    //System.Console.Write('\n');
+                    System.Console.Write(counter + " ");
+                }
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
